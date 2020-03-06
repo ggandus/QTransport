dc7c6004f199a6e18f3cb5ffbb52229e8ec71a00
ggandus <ggandus@student.ethz.ch>
2020-03-06 15:26:58 +0100

    Add func. take_bfs_activespace to CoupledHamilton

--- transport/coupledhamiltonian.py
+++ transport/coupledhamiltonian.py
@@ -2,6 +2,10 @@ import numpy as np
 from .tools import subdiagonalize, rotate_matrix, dagger, \
                    order_diagonal, cutcoupling, get_subspace
 
+
+from .tk_gpaw import subdiagonalize_atoms, get_bfs_indices, \
+                     extract_orthogonal_subspaces, flatten
+
 class CoupledHamiltonian:
 
     def __init__(self, H, S, selfenergies):
@@ -96,14 +100,68 @@ class CoupledHamiltonian:
 
       return ht_mm, st_mm, c_mm
 
+    def take_bfs_activespace(self, calc, a, key=lambda x: abs(x)<np.inf, cutoff=np.inf):
+        '''
+            key     := Apply condition to eigenvalues of subdiagonalized atoms.
+            cutoff  := Cutoff for effective embedding to incudle in active space,
+                       form subdiagonalized [a] orbitals.
+        '''
+        h_mm = self.H
+        s_mm = self.S
+        nbf = h_mm.shape[-1]
+
+        c_MM, e_aj = subdiagonalize_atoms(calc, h_mm, s_mm, a)
+        bfs_imp = get_bfs_indices(calc, a)
+        # In [a] but not in active space
+        bfs_not_m = [bfs_imp[i] for i,
+                                    eigval in enumerate(flatten(e_aj))
+                     if not key(eigval)] #Take complementary
+        # Active space
+        bfs_m = list(np.setdiff1d(bfs_imp,bfs_not_m))
+        # o := orbitals other atoms not in [a]
+        bfs_m_and_o_i = list(np.setdiff1d(range(nbf),bfs_not_m))
+        nbfs_m_and_o = len(bfs_m_and_o_i)
+
+        #Apply subdiagonalization
+        self.apply_rotation(c_MM)
+
+        if cutoff<np.inf:
+            # h_imp = get_subspace(self.H, bfs_imp)
+            # s_imp = get_subspace(self.S, bfs_imp)
+            #Effective active space := activespace and effective embedding
+            bfs_eff_i = []
+            bfs_eff_imp = []
+            for bfm in bfs_m: # for each bfm (bfm := basis of active)
+                row_m_imp = abs(self.H[bfm,bfs_imp])
+                #Index of couplings in [a]
+                coupling  = np.where(row_m_imp>cutoff)[0]
+                bfs_eff_imp = np.union1d(bfs_eff_imp, coupling)
+                #Effective active embedding for bfm
+                bfs_emb_i = np.arange(self.H.shape[0])[bfs_imp][coupling]
+                #Effective activespace
+                bfs_eff_i = np.union1d(bfs_eff_i, bfs_emb_i)
+            #Unify with others
+            bfs_eff_i = bfs_eff_i.tolist()
+            bfs_m_and_o_i = np.union1d(bfs_eff_i, bfs_m_and_o_i).tolist()
+            #Modify activespace with effective active space
+            h_imp = get_subspace(self.H, bfs_eff_i)
+            s_imp = get_subspace(self.S, bfs_eff_i)
+        else:
+            h_imp = get_subspace(self.H, bfs_m)
+            s_imp = get_subspace(self.S, bfs_m)
+
+
+        #Take (effective) activespace
+        self.take_bfs(bfs_m_and_o_i, apply=True)
+
+        return h_imp, s_imp
+
     def get_activespace(self, calc, a, key=lambda x: abs(x)<np.inf, orthogonal=True, inverse=False):
         '''
             key     := Apply condition to eigenvalues of subdiagonalized atoms.
             inverse := Get embedding instead. Put activespace in selfenergy
         '''
         from .internalselfenergy import InternalSelfEnergy
-        from .tk_gpaw import subdiagonalize_atoms, get_bfs_indices, \
-                             extract_orthogonal_subspaces, flatten
 
         h_mm = self.H
         s_mm = self.S
