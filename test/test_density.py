f6c531e95c5991168cdbda34c01cf992e5f7c91c
ggandus <ggandus@student.ethz.ch>
2020-05-09 10:46:09 +0200

    add test_density

---
+++ test/test_density.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pytest
+
+from transport.continued_fraction import integrate_dos
+from poisson.scf.tr_density_equilibrium import get_density_equilibrium as integrate_dos
+
+@pytest.mark.parametrize('prefix',['poly2'])
+@pytest.mark.parametrize('method',[#'from_gf',
+                                   #'from_hs',
+                                   'from_rgf'])
+                                   # pytest.param('from_rgf',pytest.mark.recursive)])
+def test_occupation(prefix, method, get_expected, setup):
+
+    tcalc = setup(method, prefix)
+    tcalc.initialize()
+    N = get_expected(prefix, 'nelectrons')
+    #
+    GF = tcalc.greenfunction
+    nele = integrate_dos(GF).sum()
+    assert np.allclose(nele, N)
+
+# test_transmission(setup)
