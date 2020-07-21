800d59f5a98f377d2ef658bf29461ca18b7f346b
ggandus <ggandus@student.ethz.ch>
2020-07-21 18:22:27 +0200

    checkout pointers

--- transport/lcao/cython/recursive_sgf.pyx
+++ transport/lcao/cython/recursive_sgf.pyx
@@ -17,42 +17,36 @@ cdef inline complex conj(complex a) nogil:
     return a.real - a.imag*1j
 
 
-cdef inline void mat_add(complex alpha, complex[::1,:] a, complex[::1,:] b) nogil:
-
-    cdef int m = a.shape[0], i, j
-    for j in prange(m, schedule='static'):
-        for i in range(m):
-            b[j,i] = alpha * a[j,i] + b[j,i]
-
-
-cdef inline void init_vecs(complex[:,:] h_ii, complex[:,:] s_ii,
+cdef inline void init_vecs(complex* h_ii, complex* s_ii,
                            complex[:,:] h_ij, complex[:,:] s_ij,
-                           complex[::1,:] v_00, complex[::1,:] v_01,
-                           complex[::1,:] v_10, complex[::1,:] v_11,
-                           double energy, double eta=1e-5, double bias=0.) nogil:
+                           complex* v_00, complex* v_01,
+                           complex* v_10, complex* v_11,
+                           double energy, int m,
+                           double eta=1e-5, double bias=0.) nogil:
 
-    cdef int m = h_ii.shape[0], i, j
+    cdef int i, j
     cdef complex z
     z = energy - bias + eta * 1.j
 
-    for j in prange(m, schedule='static'):
-        for i in range(m):
-            v_00[j,i] = z * conj(s_ii[i,j]) - conj(h_ii[i,j])
-            v_11[j,i] = v_00[j,i]
-            v_01[j,i] = z * conj(s_ij[i,j]) - conj(h_ij[i,j])
+    for i in prange(m, schedule='static'):
+        for j in range(m):
+            v_00[i*m+j] = z * conj(s_ii[i*m+j]) - conj(h_ii[i*m+j])
+            v_11[i*m+j] = v_00[i*m+j]
+            v_01[i*m+j] = z * conj(s_ij[i,j]) - conj(h_ij[i,j])
 
-    for j in prange(m, schedule='static'):
-        for i in range(m):
-            v_10[j,i] = z * s_ij[j,i] - h_ij[j,i]
+    for i in prange(m, schedule='static'):
+        for j in range(m):
+            v_10[i*m+j] = z * s_ij[j,i] - h_ij[j,i]
 
 
-cdef void get_Ginv(complex[::1,:] v_00, complex[::1,:] v_01,
-                   complex[::1,:] v_10, complex[::1,:] v_11,
+cdef void get_Ginv(complex* v_00, complex* v_01,
+                   complex* v_10, complex* v_11,
                    complex[::1,:] a, complex[::1,:] b,
-                   complex[::1,:] lu, complex[::1,:] v_01_dot_b, int[:] ipiv) nogil:
+                   complex[::1,:] lu, complex[::1,:] v_01_dot_b,
+                   int[:] ipiv, int m) nogil:
     # """The inverse of the retarded surface Green function"""
 
-    cdef int m = v_00.shape[0], i, j, info, inc = 1
+    cdef int i, j, info, inc = 1
     cdef int m2 = m*m
     cdef char trans = b'N', norm = b'M', UPLO = b'A'
     cdef complex alpha = 1.+0.j, beta = -1.+0.j, gamma = 0.+0.j
@@ -83,46 +77,46 @@ cdef void get_Ginv(complex[::1,:] v_00, complex[::1,:] v_01,
         # py_delta = abs(py_v_01).max()
         ###########################
 
-        zlacpy(&UPLO,&m,&m,&v_11[0,0],&m,&lu[0,0],&m)
+        zlacpy(&UPLO,&m,&m,&v_11[0],&m,&lu[0,0],&m)
         # assert np.allclose(np.asarray(v_11), np.asarray(lu)), 'copy v_11'
         # assert np.allclose(np.asarray(v_11), py_v_11), 'copy v_11'
         zgetrf(&m,&m,&lu[0,0],&m,&ipiv[0],&info)
         # assert np.allclose(np.asarray(lu), py_lu), 'lu'
-        zlacpy(&UPLO,&m,&m,&v_01[0,0],&m,&a[0,0],&m)
+        zlacpy(&UPLO,&m,&m,&v_01[0],&m,&a[0,0],&m)
         # assert np.allclose(np.asarray(a), py_v_01), 'comp a v_01'
         zgetrs(&trans,&m,&m,&lu[0,0],&m,&ipiv[0],&a[0,0],&m,&info);
         # assert np.allclose(np.asarray(a), py_a), 'a'
-        zlacpy(&UPLO,&m,&m,&v_10[0,0],&m,&b[0,0],&m)
+        zlacpy(&UPLO,&m,&m,&v_10[0],&m,&b[0,0],&m)
         zgetrs(&trans,&m,&m,&lu[0,0],&m,&ipiv[0],&b[0,0],&m,&info);
         # assert np.allclose(np.asarray(b), py_b) ,'b'
-        zgemm(&trans,&trans,&m,&m,&m,&alpha,&v_01[0,0],&m,&b[0,0],
+        zgemm(&trans,&trans,&m,&m,&m,&alpha,&v_01[0],&m,&b[0,0],
               &m,&gamma,&v_01_dot_b[0,0],&m)
         # assert np.allclose(np.asarray(v_01_dot_b), py_v_01_dot_b), 'v_01_dot_b'
         # mat_add(beta, v_01_dot_b, v_00)
-        zaxpy(&m2,&beta,&v_01_dot_b[0,0],&inc,&v_00[0,0],&inc)
+        zaxpy(&m2,&beta,&v_01_dot_b[0,0],&inc,&v_00[0],&inc)
         # assert np.allclose(np.asarray(v_00), py_v_00), 'v_00'
-        zgemm(&trans,&trans,&m,&m,&m,&beta,&v_10[0,0],&m,&a[0,0],
-              &m,&alpha,&v_11[0,0],&m)
+        zgemm(&trans,&trans,&m,&m,&m,&beta,&v_10[0],&m,&a[0,0],
+              &m,&alpha,&v_11[0],&m)
         # assert not np.allclose(np.asarray(v_11), py_v_11), 'v_11'
         # mat_add(beta, v_01_dot_b, v_11)
-        zaxpy(&m2,&beta,&v_01_dot_b[0,0],&inc,&v_11[0,0],&inc)
+        zaxpy(&m2,&beta,&v_01_dot_b[0,0],&inc,&v_11[0],&inc)
         # assert np.allclose(np.asarray(v_11), py_v_11), 'v_11'
-        zlacpy(&UPLO,&m,&m,&v_01[0,0],&m,&lu[0,0],&m)
+        zlacpy(&UPLO,&m,&m,&v_01[0],&m,&lu[0,0],&m)
         zgemm(&trans,&trans,&m,&m,&m,&beta,&lu[0,0],&m,&a[0,0],
-              &m,&gamma,&v_01[0,0],&m)
+              &m,&gamma,&v_01[0],&m)
         # assert np.allclose(np.asarray(v_01), py_v_01), 'v_01'
-        zlacpy(&UPLO,&m,&m,&v_10[0,0],&m,&lu[0,0],&m)
+        zlacpy(&UPLO,&m,&m,&v_10[0],&m,&lu[0,0],&m)
         zgemm(&trans,&trans,&m,&m,&m,&beta,&lu[0,0],&m,&b[0,0],
-              &m,&gamma,&v_10[0,0],&m)
+              &m,&gamma,&v_10[0],&m)
         # assert np.allclose(np.asarray(v_10), py_v_10), 'v_10'
-        delta = zlange(&norm,&m,&m,&v_01[0,0],&m,&work[0])
+        delta = zlange(&norm,&m,&m,&v_01[0],&m,&work[0])
 
-cdef inline void inv(complex[::1,:] a, complex[::1] work, int[:] ipiv) nogil:
+cdef inline void inv(complex* a, complex[::1] work, int[:] ipiv, int m) nogil:
 
-    cdef int m = a.shape[0], info, lwork = work.shape[0]
+    cdef int info, lwork = work.shape[0]
 
-    zgetrf(&m,&m,&a[0,0],&m,&ipiv[0],&info)
-    zgetri(&m,&a[0,0],&m,&ipiv[0],&work[0],&lwork,&info)
+    zgetrf(&m,&m,&a[0],&m,&ipiv[0],&info)
+    zgetri(&m,&a[0],&m,&ipiv[0],&work[0],&lwork,&info)
 
 
 def get_G(G_kMM,H_kii,S_kii,H_kij,S_kij,energy,eta=1e-5,bias=0.):
@@ -135,9 +129,13 @@ def get_G(G_kMM,H_kii,S_kii,H_kij,S_kij,energy,eta=1e-5,bias=0.):
     # v_xx = z * OP(h_xx) - OP(s_xx)
     # OP :: .T or .H
     v_00 = np.zeros((m,n), complex, order='F')
-    v_11 = np.zeros((m,n), complex, order='F')
-    v_10 = np.zeros((m,n), complex, order='F')
+    cdef complex[:,:] _v_00 = v_00
     v_01 = np.zeros((m,n), complex, order='F')
+    cdef complex[:,:] _v_01 = v_01
+    v_10 = np.zeros((m,n), complex, order='F')
+    cdef complex[:,:] _v_10 = v_10
+    v_11 = np.zeros((m,n), complex, order='F')
+    cdef complex[:,:] _v_11 = v_11
 
     # Loop internal vars.
     a = np.zeros((m,n), complex, order='F')
@@ -150,10 +148,19 @@ def get_G(G_kMM,H_kii,S_kii,H_kij,S_kij,energy,eta=1e-5,bias=0.):
     work = np.zeros(lwork, complex)
 
     # Green's function at transverse k-points
+    cdef complex[:,:] h_ii
+    cdef complex[:,:] s_ii
+    # cdef complex[:,:] h_ij
+    # cdef complex[:,:] s_ij
     for i in range(nkpts):
-        init_vecs(H_kii[i],S_kii[i],H_kij[i],S_kij[i],
-                  v_00,v_01,v_10,v_11,energy)
-        get_Ginv(v_00,v_01,v_10,v_11,
-                 a,b,lu,v_01_dot_b,ipiv)
-        inv(v_00,work,ipiv)
+        h_ii = H_kii[i]
+        s_ii = S_kii[i]
+        # h_ij = H_kij[i]
+        # s_ij = S_kij[i]
+        init_vecs(&h_ii[0,0],&s_ii[0,0],H_kij[i],S_kij[i],
+                  &_v_00[0,0],&_v_01[0,0],&_v_10[0,0],&_v_11[0,0],
+                  energy,m)
+        get_Ginv(&_v_00[0,0],&_v_01[0,0],&_v_10[0,0],&_v_11[0,0],
+                 a,b,lu,v_01_dot_b,ipiv,m)
+        inv(&_v_00[0,0],work,ipiv,m)
         G_kMM[i] = v_00#la.inv(v_00)#, overwrite_a=True, check_finite=False)
